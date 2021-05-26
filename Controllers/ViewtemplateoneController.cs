03b4c177cd2c61f715371c1967f5f7078cbf3468
Meeran <meeranmydeen@gmail.com>
2021-05-26 22:29:55 +0400

    Report-3

--- Controllers/ViewtemplateoneController.cs
+++ Controllers/ViewtemplateoneController.cs
@@ -188,5 +188,362 @@ namespace Ecommerce_NetCore_API.Controllers
 
         }
 
+
+        [HttpPost("prodsalehistory")]
+        public ActionResult<List<ProdSaleHistoryReportModel>> GetProductSaleHistory([FromForm] ViewTemplateModelOne data, string groupvalue)
+        {
+        bool IsFromDateValid = false;
+        bool IsEndDateValid = false;
+        DateTime FromDate;
+        string CompleteFromDateString = "";
+        DateTime EndDate;
+        string CompleteEndDateString = "";
+        DateTime dt;
+        bool SelectOne = false;
+        bool SelectTwo = false;
+        bool SelectThree = false;
+        List<SalewithCustIdTE> products = new List<SalewithCustIdTE>();
+        List<ProdSaleHistoryReportModel> ReportModelList = new List<ProdSaleHistoryReportModel>();
+        
+        string Result = "";
+        bool NotFound = false;
+        bool IsGroup = groupvalue == "GROUP" ? true : false;
+
+            if (data.FromDate != null && data.EndDate != null)
+            {
+                
+                DateService DateService1 = new DateService();
+        CompleteFromDateString = DateService1.GetCompleteDate(data.FromDate);
+                IsFromDateValid = DateTime.TryParse(CompleteFromDateString, out dt);
+
+                DateService DateService2 = new DateService();
+        CompleteEndDateString = DateService2.GetCompleteDate(data.EndDate);
+                IsEndDateValid = DateTime.TryParse(CompleteEndDateString, out dt);
+            }
+            if(IsFromDateValid && IsEndDateValid)
+            {
+                FromDate = Convert.ToDateTime(CompleteFromDateString);
+                EndDate =  Convert.ToDateTime(CompleteEndDateString);
+                if(data.CategoryValue != null && data.ProductValue == null && data.SizeValue == null)
+                {
+                    SelectOne = true;
+                    var ProductsByCategoryId = context.products.Where(x => x.CategoryId.ToString() == data.CategoryValue).ToList();
+                    if(ProductsByCategoryId.Count() != 0)
+                    {
+                        
+                        foreach(var ProductsById in ProductsByCategoryId)
+                        { 
+                            var collection = context.saleswithCustomerIds.Where(x => x.Productid == ProductsById.Id && x.Purchasedate >= FromDate && x.Purchasedate <= EndDate).OrderBy(x => x.Prodsize).ToList();
+                            foreach(var prod in collection)
+                            {
+                                products.Add(prod);
+                            }
+                        }
+                       // return Ok(products);
+                    }
+                    else
+                    {
+                        Result = "NotFound";
+                        NotFound = true;
+                    }
+
+                }
+                else if (data.ProductValue != null && data.SizeValue == null)
+                {
+                    SelectTwo = true;
+                    var DataCollection = context
+                           .saleswithCustomerIds
+                           .Where(x => x.Productid.ToString() == data.ProductValue && x.Purchasedate >= FromDate && x.Purchasedate <= EndDate).OrderBy(x => x.Prodsize).ToList();
+                    if (DataCollection.Count() != 0)
+                    {
+
+                        products = DataCollection;
+                    }
+                    else
+                    {
+                        Result = "NotFound";
+                        NotFound = true;
+                    }
+
+                }
+                else if (data.ProductValue != null && data.SizeValue != null)
+                {
+                    SelectThree = true;
+                    var DataBySize = context.saleswithCustomerIds
+                        .Where(x => x.Productid.ToString() == data.ProductValue && x.Prodsize == data.SizeValue && x.Purchasedate >= FromDate && x.Purchasedate <= EndDate).ToList();
+                    if (DataBySize.Count() > 0)
+                    {
+                        products = DataBySize;
+
+                    }
+                    else
+                    {
+                        Result = "NotFound";
+                        NotFound = true;
+
+                    }
+
+                }
+
+                if (products.Count() > 0 && SelectOne && IsGroup)
+                {
+                    var GroupBySizeSelectOne = products.GroupBy(x => new { x.Productid, x.Prodsize }).Select(z => new SalewithCustIdTE()
+                    {
+                        Productid = z.Key.Productid,
+                        Prodsize = z.Key.Prodsize,
+                        Quantity = z.Sum(x => x.Quantity),
+                        Unitprice = z.Sum(x => x.Unitprice) / z.Count(),
+                    });
+                    products = GroupBySizeSelectOne.ToList();
+
+                }
+
+                else if (products.Count() > 0 && SelectTwo && IsGroup)
+                {
+                    var GroupBySelectTwo = products.GroupBy(x => x.Prodsize).Select(z => new SalewithCustIdTE()
+                    {
+                        Productid = products[0].Productid,
+                        Prodsize = z.Key,
+                        Quantity = z.Sum(x => x.Quantity),
+                        Unitprice = z.Sum(x => x.Unitprice) / z.Count(),
+                    });
+                    products = GroupBySelectTwo.ToList();
+                }
+
+                else if (products.Count() > 0 && SelectThree && IsGroup)
+                {
+                    SalewithCustIdTE SelectThreeData = new SalewithCustIdTE()
+                    {
+                        Productid = products[0].Productid,
+                        Prodsize = products[0].Prodsize,
+                        Quantity = products.Sum(x => x.Quantity),
+                        Unitprice = products.Sum(x => x.Unitprice) / products.Count()
+
+                    };
+                    products.Clear();
+                    products.Add(SelectThreeData);
+                }
+
+                //Produt Name not avilable in products thats why converting into new Model with productName!!
+                if (products.Count() > 0)
+                {
+                    foreach (var prod in products)
+                    {
+                        ProductNameService NameService = new ProductNameService(context);
+                        ProdSaleHistoryReportModel model = new ProdSaleHistoryReportModel()
+                        {
+                            Productname = NameService.GetProductName(prod.Productid),
+                            Size = prod.Prodsize,
+                            Quantity = prod.Quantity,
+                            Cost = prod.Unitprice,
+                            Date = prod.Purchasedate,
+                            Productid = prod.Productid
+
+                        };
+                        ReportModelList.Add(model);
+                    }
+
+                }
+
+                else
+                {
+                    NotFound = true;
+                }
+
+            }
+            if (NotFound)
+            {
+                return Ok(Result);
+            }
+            
+            return Ok(ReportModelList);
+
+        }
+
+        [HttpPost("prodsaleprofit")]
+        public ActionResult<List<ProdProfitReportModel>>GetProductSaleProfit([FromForm]ViewTemplateModelOne data)
+        {
+            bool IsFromDateValid = false;
+            bool IsEndDateValid = false;
+            DateTime FromDate;
+            string CompleteFromDateString = "";
+            DateTime EndDate;
+            string CompleteEndDateString = "";
+            DateTime dt;
+            bool SelectOne = false;
+            bool SelectTwo = false;
+            bool SelectThree = false;
+            List<SalewithCustIdTE> products = new List<SalewithCustIdTE>();
+            List<ProdProfitReportModel> ReportModel = new List<ProdProfitReportModel>();
+            string Result = "";
+            bool NotFound = false;
+            if (data.FromDate != null && data.EndDate != null)
+            {
+
+                DateService DateService1 = new DateService();
+                CompleteFromDateString = DateService1.GetCompleteDate(data.FromDate);
+                IsFromDateValid = DateTime.TryParse(CompleteFromDateString, out dt);
+
+                DateService DateService2 = new DateService();
+                CompleteEndDateString = DateService2.GetCompleteDate(data.EndDate);
+                IsEndDateValid = DateTime.TryParse(CompleteEndDateString, out dt);
+            }
+            if (IsFromDateValid && IsEndDateValid)
+            {
+                FromDate = Convert.ToDateTime(CompleteFromDateString);
+                EndDate = Convert.ToDateTime(CompleteEndDateString);
+
+                if (data.CategoryValue != null && data.ProductValue == null && data.SizeValue == null)
+                {
+                    SelectOne = true;
+                    var ProductsByCategoryId = context.products.Where(x => x.CategoryId.ToString() == data.CategoryValue).ToList();
+                    if (ProductsByCategoryId.Count() != 0)
+                    {
+
+                        foreach (var ProductsById in ProductsByCategoryId)
+                        {
+                            var collection = context.saleswithCustomerIds.Where(x => x.Productid == ProductsById.Id && x.Purchasedate >= FromDate && x.Purchasedate <= EndDate).OrderBy(x => x.Prodsize).ToList();
+                            foreach (var prod in collection)
+                            {
+                                products.Add(prod);
+                            }
+                        }
+                        // return Ok(products);
+                    }
+                    else
+                    {
+                        Result = "NotFound";
+                        NotFound = true;
+                    }
+
+                }
+                else if (data.ProductValue != null && data.SizeValue == null)
+                {
+                    SelectTwo = true;
+                    var DataCollection = context
+                           .saleswithCustomerIds
+                           .Where(x => x.Productid.ToString() == data.ProductValue && x.Purchasedate >= FromDate && x.Purchasedate <= EndDate).OrderBy(x => x.Prodsize).ToList();
+                    if (DataCollection.Count() != 0)
+                    {
+
+                        products = DataCollection;
+                    }
+                    else
+                    {
+                        Result = "NotFound";
+                        NotFound = true;
+                    }
+
+                }
+                else if (data.ProductValue != null && data.SizeValue != null)
+                {
+                    SelectThree = true;
+                    var DataBySize = context.saleswithCustomerIds
+                        .Where(x => x.Productid.ToString() == data.ProductValue && x.Prodsize == data.SizeValue && x.Purchasedate >= FromDate && x.Purchasedate <= EndDate).ToList();
+                    if (DataBySize.Count() > 0)
+                    {
+                        products = DataBySize;
+
+                    }
+                    else
+                    {
+                        Result = "NotFound";
+                        NotFound = true;
+
+                    }
+
+                }
+                List<SalewithCustIdTE> ProdSaleGropued = new List<SalewithCustIdTE>();
+                if (products.Count() > 0 && SelectOne)
+                {
+                    var GroupBySizeSelectOne = products.GroupBy(x => new { x.Productid, x.Prodsize }).Select(z => new SalewithCustIdTE()
+                    {
+                        Productid = z.Key.Productid,
+                        Prodsize = z.Key.Prodsize,
+                        Quantity = z.Sum(x => x.Quantity),
+                        Unitprice = z.Sum(x => x.Unitprice) / z.Count(),
+                    });
+                    ProdSaleGropued = GroupBySizeSelectOne.ToList();
+
+                }
+
+                else if (products.Count() > 0 && SelectTwo)
+                {
+                    var GroupBySelectTwo = products.GroupBy(x => x.Prodsize).Select(z => new SalewithCustIdTE()
+                    {
+                        Productid = products[0].Productid,
+                        Prodsize = z.Key,
+                        Quantity = z.Sum(x => x.Quantity),
+                        Unitprice = z.Sum(x => x.Unitprice) / z.Count(),
+                    });
+                    ProdSaleGropued = GroupBySelectTwo.ToList();
+                }
+
+                else if (products.Count() > 0 && SelectThree)
+                {
+                    SalewithCustIdTE SelectThreeData = new SalewithCustIdTE()
+                    {
+                        Productid = products[0].Productid,
+                        Prodsize = products[0].Prodsize,
+                        Quantity = products.Sum(x => x.Quantity),
+                        Unitprice = products.Sum(x => x.Unitprice) / products.Count()
+
+                    };
+                    ProdSaleGropued.Clear();
+                    ProdSaleGropued.Add(SelectThreeData);
+                }
+
+                // Creating Profit Model for Result
+                if(ProdSaleGropued.Count()> 0)
+                {
+                    List<ProdAddHistoryTE> ProductsAll = new List<ProdAddHistoryTE>();
+                    foreach(SalewithCustIdTE prod in ProdSaleGropued)
+                    {
+                   List<ProdAddHistoryTE> ProdRegistered = context
+                            .prodAddHistoryData
+                            .Where(x => x.ProductId == prod.Productid && x.Size == prod.Prodsize).ToList();
+                       foreach(var prodSingle in ProdRegistered)
+                        {
+                            ProductsAll.Add(prodSingle);
+                        }
+                    }
+
+                    if(ProductsAll.Count() > 0)
+                    {
+                        List<ProdAddHistoryTE> ProdRegGrouped = ProductsAll.GroupBy(x => new { x.ProductId, x.Size }).Select(z => new ProdAddHistoryTE()
+                        {
+                            ProductId = z.Key.ProductId,
+                            Size = z.Key.Size,
+                            Cost = z.Sum(x => x.Cost) / z.Count(),
+                            Quantity = z.Sum(x => x.Quantity),
+
+                        }).ToList<ProdAddHistoryTE>();
+                        foreach(var ProductSold in ProdSaleGropued)
+                        {
+                            var ProductRegistered =  ProdRegGrouped.Single(x => x.ProductId == ProductSold.Productid && x.Size == ProductSold.Prodsize);
+                            ProductNameService NameService = new ProductNameService(context);
+                            ProdProfitReportModel model = new ProdProfitReportModel()
+                            {
+                              Productname = NameService.GetProductName(ProductSold.Productid),
+                              Size = ProductSold.Prodsize,
+                              Purchasecostaverage = ProductRegistered.Cost,
+                              Salecostaverage = ProductSold.Unitprice,
+                              Quantitysold =  ProductSold.Quantity,
+                              Profit = ProductSold.Quantity * ProductSold.Unitprice - ProductSold.Quantity * ProductRegistered.Cost,
+                            };
+                            ReportModel.Add(model);
+                        }
+                    
+                    }
+
+                }
+            }
+               if(NotFound)
+               {
+                return Ok(Result);
+               }
+            return Ok(ReportModel);
+        }
+
     }
 }
---
+++ Models/ProdProfitReportModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ecommerce_NetCore_API.Models
+{
+    public class ProdProfitReportModel
+    {
+        public string Productname { get; set; }
+        public string Size { get; set; }
+        public int Purchasecostaverage { get; set; }
+        public int Salecostaverage { get; set; }
+        public int Quantitysold { get; set; }
+        public int Profit { get; set; }
+    }
+}
